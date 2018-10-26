e327e18ccbb8fd75c183c87d0d86c3274414a3d4
Valerie <valerie_sharp93@yahoo.com>
2018-10-26 12:37:37 -0700

    Pushing project files

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Nav></Nav>
+    <div id="app">
+      <img src="@/assets/img/logo.svg">
+      <router-view/>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  import Nav from '@/components/Nav'
+export default {
+    name: 'app',
+    components: {
+      Nav
+    }
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  /*padding-top: 60px;*/
+}
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="nav">
+    <header class="mdc-top-app-bar">
+      <div class="mdc-top-app-bar__row">
+        <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+          <a href="#" class="material-icons mdc-top-app-bar__navigation-icon">menu</a>
+          <span class="mdc-top-app-bar__title"><Logo></Logo></span>
+        </section>
+      </div>
+    </header>
+  </div>
+
+</template>
+
+<script>
+  import Logo from '@/components/Logo'
+  export default {
+    name: 'Nav',
+    components: {
+      Logo
+    }
+  }
+</script>
+
+<style scoped>
+  #nav{
+    padding-bottom: 120px;
+  }
+  .mdc-top-app-bar__title img{
+    width: 100%;
+
+  }
+</style>
